964a1845192df1765fb89e7663744d780ced18ba
M Pradyumna <mprc9125@gmail.com>
2025-11-27 13:37:45 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Navigation from '@/components/Navigation';
-import Hero from '@/components/Hero';
-import About from '@/components/About';
-import Skills from '@/components/Skills';
-import Projects from '@/components/Projects';
-import Timeline from '@/components/Timeline';
-import Contact from '@/components/Contact';
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Timeline from './components/Timeline';
+import Contact from './components/Contact';
 
 export default function Home() {
   return (
